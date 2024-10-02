383a37869a2ddacb9c12944d354a57e51d7c6e6a
Nicholas Potenza <nicholas.potenza@outlook.com>
2024-10-01 23:08:44 -0500

    Add project files.

---
+++ Challengelab6.2/MissingNum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challengelab6._2
+{
+    internal class MissingNum
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 0, 1 };
+            int missingnum = MissingNumber(nums);
+
+            Console.WriteLine($"The missing number is: {missingnum}");
+
+            int[] nums1 = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            int missingnum1 = MissingNumber(nums1);
+
+            Console.WriteLine($"The missing number is: {missingnum1}");
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+            int size = nums.Length + 1;
+            int[] ints = new int[size];
+
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                ints[nums[i]] = -1;
+            }
+
+            for (int i = 0; i < size; ++i)
+            {
+                if (ints[i] > -1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Challengelab6/FindOnce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challengelab6
+{
+    internal class FindOnce
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 2, 1 };
+            int singleNumber = SingleNumber(nums);
+
+            Console.WriteLine($"The single number is: {singleNumber}");
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+
+            int j = 0;
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            else if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+            else
+            {
+                for (int i = 1; i < nums.Length; i++)
+                {
+
+                    if (nums[j] == nums[i])
+                    {
+                        int temp = nums[j + 1];
+                        nums[j + 1] = nums[i];
+                        nums[i] = temp;
+                        j = j + 2;
+                        i = j;
+                    }
+                }
+            }
+            return nums[j];
+        }
+    }
+}
